d34b6939979a42288bf162819bc1473602dedc1e
jeevan437 <jeevanpjr@gmail.com>
2019-12-13 16:57:09 +0530

    performed CRUD operatins and added static files

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import url
 
-from .views import get_stores,add_stores,success,home
+from .views import get_stores,add_stores,success,home,delete_store,store_update
 
 urlpatterns = [
 
-    url(r'home/',home),
+    url(r'home/',home, name='home'),
     url(r'get_stores',get_stores, name = 'store_details'),
     url(r'add_stores',add_stores,name = 'add_stores'),
-    url(r'success',success,name='success')
+    url(r'success',success,name='success'),
+
+    url(r'delete/(?P<pid>[\d]+)/$', delete_store),
+    url(r'update/(?P<pid>[\d]+)/$', store_update),
 
 
 ]
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render,reverse,redirect
 from django.http import HttpResponseRedirect
 from .models import Stores
+from django.db.models import Q
 from .forms import StoreForm
+from django.core.paginator import Paginator
 # Create your views here.
 
 def home(request):
     return render(request,'base.html')
+
 def get_stores(request):
+    if request.method == 'GET':
+        data = Stores.objects.all().order_by('id')
+        search_data = request.GET.get('search_store')
 
-    stores_data = Stores.objects.all()
+        if search_data is not None:
+            data = data.filter(Q(name__icontains=search_data) | Q(address__icontains = search_data ) ).order_by('id')
+        return render(request, 'get_stores.html',
+                      {'stores_data':data,
 
-    return render(request, "get_stores.html", {'stores_data':stores_data})
+                       })
 
+
+'''
 def add_stores(request):
     if request.method == 'POST':
         form = StoreForm(request.POST)
@@ -30,7 +41,52 @@ def add_stores(request):
         form = StoreForm()
 
     return render(request, 'add_stores.html', {'form': form})
+'''
+def add_stores(request):
+    if request.method == "POST":
+        data = request.POST
+        task = Stores(name = data.get("name"),
+                            email = data.get("email"),
+                            contact = data.get("contact"),
+                            address = data.get("address"),
+                            reviews = data.get("reviews"),
+                      )
+        print(task)
+        task.save()
+        return redirect(get_stores)
+    return render(request, "add_stores.html")
 
 def success(request):
-
     return render(request,"success.html",)
+
+def delete_store(request, pid):
+    task = Stores.objects.get(id = pid)
+    task.delete()
+    return redirect(get_stores)
+'''
+def store_update(request, pid):
+    store = Stores.objects.get(id = pid)
+    if request.method == "post":
+        data = request.POST
+        store.name = data.get("name")
+        store.email = data.get("email")
+        store.contact = data.get("contact")
+        store.address = data.get("address")
+        store.reviews = data.get("reviews")
+        store.save()
+        return redirect(get_stores)
+    return render(request, "update.html", {'store_data':store})
+
+'''
+def store_update(request, pid):
+    store_id = int(pid)
+    try:
+        stores_data = Stores.objects.get(id = store_id)
+    except Stores.DoesNotExist:
+        return redirect('home')
+    store_form = StoreForm(request.POST or None, instance = stores_data)
+    if store_form.is_valid():
+       store_form.save()
+       return redirect(get_stores)
+    return render(request, 'update.html', {'store_form':store_form})
+
